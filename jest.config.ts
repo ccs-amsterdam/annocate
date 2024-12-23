7c21cd35dddb9f1be5f93dff7e2e42a28ba21f7a
Kasper Welbers <kasperwelbers@gmail.com>
2024-12-23 13:55:42 +0100

    just some notes

--- jest.config.ts
+++ jest.config.ts
@@ -8,7 +8,7 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const config: Config = {
-  setupFilesAfterEnv: ["./setupJestMock.ts"],
+  setupFilesAfterEnv: ["./setupJest.ts"],
   coverageProvider: "v8",
   testEnvironment: "jsdom",
   // Add more setup options before each test is run
--- setupJest.ts
+++ setupJest.ts
@@ -1,7 +1,9 @@
 import { queryClient } from "@/drizzle/drizzle";
 
-var COOKIES: Record<string, string> = {};
+// MOCKING NEXT HEADERS: We need to mock any next/headers that next injects within the "request scope".
 
+// MOCKING COOKIES: We need cookies to be functional, because they are used for annotator sessions
+var COOKIES: Record<string, string> = {};
 jest.mock("next/headers", () => ({
   cookies: jest.fn(() => ({
     get: (name: string) => {
@@ -15,7 +17,11 @@ jest.mock("next/headers", () => ({
   })),
 }));
 
+// TEARDOWN
+
 afterAll(() => {
+  // We need to close the db connection after all tests are done
   // how to close depends on the client. "end" works for pg, but not for Neon (which shouldn't be used for testing)
+  // (and this way typescript is happy)
   if ("end" in queryClient) queryClient.end();
 });
